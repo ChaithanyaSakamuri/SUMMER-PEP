e46327ff76f3c1a2463fbacdb98fe193ddd2d830
Sakamuri Chaithanya Sudheer <153195215+ChaithanyaSakamuri@users.noreply.github.com>
2026-07-03 11:26:27 +0530

    3 july two

--- 3 JULY/promise.js
+++ 3 JULY/promise.js
@@ -1,35 +1,76 @@
-function getProducts() {
-    return new Promise((resolve, reject) => {
+// function getProducts() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Product loaded");
+//         }, 1000);
+//     });
+// }
+
+// function getOffers() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject("Offer request failed");
+//         }, 2000);
+//     });
+// }
+
+// function getCategories() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             reject("Categories request failed");
+//         }, 1500);
+//     });
+// }
+
+// Promise.allSettled([
+//     getProducts(),
+//     getOffers(),
+//     getCategories()
+// ])
+// .then((data) => {
+//     console.log("Resolved:", data);
+// })
+// .catch((err) => {
+//     console.log("Error:", err);
+// });
+
+function getUser() {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("Product loaded");
+            resolve("User Loaded");
         }, 1000);
     });
 }
 
-function getOffers() {
-    return new Promise((resolve, reject) => {
+function getProducts() {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            reject("Offer request failed");
+            resolve("Products Loaded");
         }, 2000);
     });
 }
 
-function getCategories() {
-    return new Promise((resolve, reject) => {
+function getOrderDetail() {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            reject("Categories request failed");
-        }, 1500);
+            resolve("Order Details Loaded");
+        }, 3000);
     });
 }
 
-Promise.allSettled([
-    getProducts(),
-    getOffers(),
-    getCategories()
-])
-.then((data) => {
-    console.log("Resolved:", data);
-})
-.catch((err) => {
-    console.log("Error:", err);
-});
+async function loadData() {
+    console.log("Hello World");
+
+    const user = await getUser();
+    console.log(user);
+
+    const products = await getProducts();
+    console.log(products);
+
+    const order = await getOrderDetail();
+    console.log(order);
+}
+
+loadData();
+
+console.log("Hello");
