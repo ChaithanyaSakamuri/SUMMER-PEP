ce5f9e19649cdcc80a66b1a110c4ef93b6b3be74
Sakamuri Chaithanya Sudheer <153195215+ChaithanyaSakamuri@users.noreply.github.com>
2026-07-03 11:33:44 +0530

    3 july three

--- 3 JULY/promise.js
+++ 3 JULY/promise.js
@@ -34,43 +34,92 @@
 //     console.log("Error:", err);
 // });
 
-function getUser() {
+// function getUser() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("User Loaded");
+//         }, 1000);
+//     });
+// }
+
+// function getProducts() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("Products Loaded");
+//         }, 2000);
+//     });
+// }
+
+// function getOrderDetail() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("Order Details Loaded");
+//         }, 3000);
+//     });
+// }
+
+// async function loadData() {
+//     console.log("Hello World");
+
+//     const user = await getUser();
+//     console.log(user);
+
+//     const products = await getProducts();
+//     console.log(products);
+
+//     const order = await getOrderDetail();
+//     console.log(order);
+// }
+
+// loadData();
+
+// console.log("Hello");
+
+function login() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("User Loaded");
+            console.log("1. Logging in...");
+            console.log("2. Login successful");
+            resolve();
         }, 1000);
     });
 }
 
-function getProducts() {
+function getUser() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("Products Loaded");
-        }, 2000);
+            console.log("3. Getting user...");
+            console.log("4. User is Loaded");
+            resolve();
+        }, 1000);
     });
 }
 
-function getOrderDetail() {
+function getOrders() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("Order Details Loaded");
-        }, 3000);
+            console.log("5. Getting orders...");
+            console.log("6. Orders data is Loaded");
+            resolve();
+        }, 1000);
+    });
+}
+
+function getOrderDetails() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("7. Getting order details...");
+            console.log("8. Order details are Loaded");
+            resolve();
+        }, 1000);
     });
 }
 
 async function loadData() {
-    console.log("Hello World");
-
-    const user = await getUser();
-    console.log(user);
-
-    const products = await getProducts();
-    console.log(products);
-
-    const order = await getOrderDetail();
-    console.log(order);
+    await login();
+    await getUser();
+    await getOrders();
+    await getOrderDetails();
 }
 
 loadData();
-
-console.log("Hello");
